6b31efac075829c7632ae383761a05867aab4cef
akaashsidhu <akaash@LAPTOP-T7CIAF8K.localdomain>
2019-12-28 11:58:55 -0500

    troubleshoot'd errors

--- skin_cancer/model_development/model.py
+++ skin_cancer/model_development/model.py
@@ -1,4 +1,3 @@
-import numpy as np
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
@@ -8,7 +7,7 @@ from sklearn.model_selection import train_test_split
 
 
 class Model:
-    def __init__(self):
+    def __init__(self, X, y):
         # Model parameters
         self.epochs = 50
         self.batch_size = 100
@@ -20,14 +19,14 @@ class Model:
                                                          verbose=1,
                                                          factor=0.5,
                                                          min_lr=0.00001)
-        self.X: np.array
-        self.y: np.array
+        self.X = X
+        self.y = y
 
         self.model: Sequential
 
     def split(self):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.20, random_state=42)  # noqa
-        self.X_train, self.X_validate, self.y_train, self.y_test = train_test_split(self.X_train, self.y_train, test_size=0.10, random_state=42)  # noqa
+        self.X_train, self.X_validate, self.y_train, self.y_validate = train_test_split(self.X_train, self.y_train, test_size=0.10, random_state=42)  # noqa
 
     def define_model(self):
         model = Sequential()
@@ -54,14 +53,15 @@ class Model:
         model.compile(optimizer='adam',
                       loss='categorical_crossentropy',
                       metrics=['accuracy'])
+        return model
 
     # Entry point to training the model
     # Consumes data from DataPipeline
-    def model_runner(self, data):
-        self.reshape_data(data)
+    def model_runner(self):
+        self.split()
         self.model = self.define_model()
         self.model.fit(self.X_train, self.y_train,
                        batch_size=self.batch_size, epochs=self.epochs,
                        validation_data=(self.X_validate, self.y_validate),
                        callbacks=[self.learning_rate_reduction])
-        self.model.evaluate(self.X_test, self.y_test, verbose=1)
+        # self.model.evaluate(self.X_test, self.y_test, verbose=1)
